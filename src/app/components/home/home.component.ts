f8d62750d087e4a782a117725ab0dad2ee1aee0f
Javier <buenorz@hotmail.com>
2021-01-20 16:58:42 +0100

    primer día

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  variable: string = "texto de prueba";
+  num1: number = 0;
+  num2: number = 0;
+  letras: string[] = ["a","b","c"];
+  usuarios = [
+    {nombre: "Manolo", apellido: "López"},
+    {nombre: "Rocío", apellido: "Dúrcal"},
+    {nombre: "Antonio", apellido: "Molina"}
+  ];
+
+  usuarioSeleccionado: object = null;
+
+  selUser(usuario): void{
+    this.usuarioSeleccionado = usuario;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
